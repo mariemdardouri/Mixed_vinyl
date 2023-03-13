e85635805535d28ea260bf7fef3cfeec8e8b7ca7
Faycel CHAOUA <faycelchaoua@gmail.com>
2023-03-13 10:43:42 +0100

    route -> controller -> response -> profit

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+
+class VinylController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response("<strong>Hello Symfony !</strong>");
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
+    }
+}
