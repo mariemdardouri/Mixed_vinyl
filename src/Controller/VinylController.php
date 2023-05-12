15ab8111e4b82709a5cdd7a6fb1b6d6c4c478be8
Mariem Dardouri <mariemdr99@gmail.com>
2023-05-12 10:58:51 +0100

    The Query Builder

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-
-
 use App\Repository\VinylMixRepository;
-use App\Service\MixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +12,6 @@ class VinylController extends AbstractController
 {
     public function __construct(
         private bool $isDebug,
-        private MixRepository $mixRepository
     )
     {}
 
@@ -42,7 +38,7 @@ class VinylController extends AbstractController
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
-        $mixes = $mixRepository->findBy([], ['votes' => 'DESC']);
+        $mixes = $mixRepository->findAllOrderedByVotes();
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
